cbfe79180ae32126c15baaade5cad30f58549dd5
Erick Cintra <developer.erickcintra@outlook.com>
2022-05-24 21:59:44 -0300

    adding turn logic

--- js/index.js
+++ js/index.js
@@ -1,40 +1,142 @@
 const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+let computerDeck = [];
 let deck = [];
+
+let gameEnd = false;
+let computerScore = 0;
 let score = 0;
 
+let playerRound = true;
+
 function getSuit() {
-    var randomValue = Math.floor(Math.random()*suits.length);
-    return suits[randomValue];
+  var randomValue = Math.floor(Math.random() * suits.length);
+  return suits[randomValue];
 }
 
-function getValue(){
-    var randomValue = Math.floor(Math.random() * 13) + 1;
-    return randomValue;
+function getValue() {
+  var randomValue = Math.floor(Math.random() * 13) + 1;
+  return randomValue;
 }
 
 function pushCard() {
+  if (!gameEnd) {
+    gameEnd = true;
+
+    getHand();
+    generateComputerHand();
+
+    if (score == 21) {
+      reset();
+    }
+    return;
+  } else if (playerRound === false) {
+    alert('voce ja jogou, espera ai');
+  } else if (deck.length < 4) {
     const cardValue = getValue();
     const suitValue = getSuit();
-    deck.push([suitValue, cardValue]);
 
+    deck.push([suitValue, cardValue]);
     generateCard(suitValue, cardValue);
-    console.log(deck)
-    scoreCounter(cardValue);
+    updateCounter(cardValue);
+  } else {
+    alert('voce ja tem 4 cartas, descarte uma');
+  }
+}
+
+function getHand() {
+  while (deck.length < 3) {
+    const cardValue = getValue();
+    const suitValue = getSuit();
+
+    deck.push([suitValue, cardValue]);
+    generateCard(suitValue, cardValue);
+
+    updateCounter(cardValue);
+  }
+}
+
+function generateComputerHand() {
+  while (computerDeck.length < 3) {
+    const card = document.createElement('div');
+    card.classList.add('computerCardHidden');
+
+    const cardValue = getValue();
+    const suitValue = getSuit();
+
+    computerDeck.push([suitValue, cardValue]);
+    document.getElementById('computer').appendChild(card);
+    computerScore = computerScore + cardValue;
+  }
+  console.log('computer score ' + computerScore);
 }
 
 function generateCard(suit, cardValue) {
-    const card = document.createElement("div");
+  const card = document.createElement('div');
 
-    card.classList.add(suit, cardValue);
-    card.style.backgroundPositionX = `${-100 * cardValue + 100}px`;
-    document.getElementById("main").appendChild(card);
+  card.classList.add(suit, cardValue);
+  card.style.zIndex = `${suits.length}`;
+  card.style.backgroundPositionX = `${-100 * cardValue + 100}px`;
+
+  card.addEventListener(
+    'click',
+    function () {
+      if (deck.length > 3) {
+        let index = deck.indexOf(
+          deck.find(
+            (cardDeck) => cardDeck[0] === suit && cardDeck[1] === cardValue
+          )
+        );
+        deck.splice(index, 1);
+        card.remove();
+        releaseCard(cardValue);
+        console.log(cardValue);
+        stack.appendChild = card;
+        document.getElementById('stack').style.backgroundPositionX = ` ${
+          -100 * cardValue + 100
+        }px`;
+        document.getElementById('stack').classList.add(suit);
+        playerRound = false;
+      }
+    },
+    false
+  );
+
+  document.getElementById('deck').appendChild(card);
 }
 
-
-function scoreCounter(cardValue) {
+function updateCounter(cardValue) {
+  if (cardValue > 10) {
+    score = score + 10;
+  } else {
     score = score + cardValue;
+  }
+  const counter = (document.getElementById('counter').innerHTML = score);
+}
 
-    if(score > 21){
-        console.log("you lost " + score)
-    }
+function formatValue(value) {
+  if (value > 10) {
+    return 10;
+  } else {
+    return value;
+  }
+}
+
+function releaseCard(cardValue) {
+  if (cardValue > 10) {
+    score = score - 10;
+  } else {
+    score = score - cardValue;
+  }
+  const counter = (document.getElementById('counter').innerHTML = score);
+}
+
+function reset() {
+  alert('quem embaralhou isso aqui?');
+  gameEnd = false;
+  deck = [];
+  computerDeck = [];
+  document.getElementById('deck').innerHTML = '';
+  document.getElementById('counter').innerHTML = '';
+  score = 0;
+  return;
 }
